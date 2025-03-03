fc05d463aa7561d9060fdd2b0ddfee83b0392d59
RAKSHITHA-M917 <rakshithar027@gmail.com>
2025-03-03 15:27:58 +0530

    Update prog1.java

--- prog1.java
+++ prog1.java
@@ -4,10 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = reader.nextInt();
 
-        System.out.println("You entered: " + number);
+        System.out.println("You entered: ");
     }
 }
